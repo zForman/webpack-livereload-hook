e0f6212c3e86aab793e867c79be56e166b7f07d8
Anton Fomin <afomin@MacBook-Pro-Anton.local>
2018-11-22 13:19:52 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import { cons, car, cdr, toString } from 'hexlet-pairs'
+//
+// const pair = cons(3, 5)
+// car(pair) // 3
+// console.log(cdr(pair))
+// toString(pair) // (3, 5)
+
+console.log('hell1o')
+let xss = 'ada'
+console.log(xss)
